ad57d8b69f2d73bfef4fa727ac10c6642701fed0
repl.it user <replituser@example.com>
2020-10-10 10:32:48 +0000

    Readdy2

--- src/exercise2/SumOfDigits.java
+++ src/exercise2/SumOfDigits.java
@@ -7,13 +7,16 @@ public class SumOfDigits {
 
     public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
+
+
+
       System.out.print("eine Zahl zwischen 0 und 999 eingeben: ");
         
-        int number=input.nextInt();
+        int number = input.nextInt();
         int a = number % 10;
         int b = (number - a) / 10 % 10;
         int c = (number - b * 10 - a) / 100 % 10;
         int sum = a + b + c;
-        System.out.println(sum);
+        System.out.println("Sum of digits: " + sum);
     }
 }
